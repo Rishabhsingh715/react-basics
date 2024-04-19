8ebef0e101642102492427ff081408f7082d9e48
Rishabh Singh <rsingh@edvak.com>
2024-04-19 18:32:57 +0530

    basic react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Teddy, Teddy2} from './components/Firstcomp.jsx'
+
+function App() {
+
+  return (
+   <>
+  <Teddy></Teddy>
+  <Teddy2></Teddy2>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Firstcomp.jsx
@@ -0,0 +1,20 @@
+function Teddy(){
+
+    return (
+        <>
+        <h1>I am the first teddy bear</h1>
+        </>
+    )
+
+}
+function Teddy2(){
+
+    return (
+        <>
+        <h1>I am the badaaa teddy bear</h1>
+        </>
+    )
+
+}
+
+export {Teddy,Teddy2};
