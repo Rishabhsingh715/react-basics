c6c8635d077033401061953675ca36a5369a0e06
Rishabh Singh <rsingh@edvak.com>
2024-04-20 11:03:49 +0530

    added hooks to project

--- src/components/Firstcomp.jsx
+++ src/components/Firstcomp.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 function Teddy(){
+
+    let [score, setScore] = useState(10)
+
+    function incScore(){
+        setScore(++score)
+    }
 
     return (
         <>
-        <h1>I am the first teddy bear</h1>
+        <h1 onClick={incScore}>I am the first teddy bear</h1>
+        <p>Score: {score}</p>
         </>
     )
 
